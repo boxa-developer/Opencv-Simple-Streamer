1a95636bb889982ba082faf19d6e3e53b5e13e34
baxodir <boxa.developer@gmail.com>
2020-09-26 15:48:20 +0500

    Update

---
+++ cv_streamer/cv_streamer/camera_views.py
@@ -0,0 +1,44 @@
+import cv2
+import numpy as np
+from django.http import StreamingHttpResponse
+import threading
+
+from django.views.decorators import gzip
+
+
+class VideoCamera(object):
+    def __init__(self):
+        url = 'rtsp://admin:parol12345@192.168.4.6:554/cam/realmonitor?channel=1&subtype=0'
+        self.video = cv2.VideoCapture(url)
+        (self.grabbed, self.frame) = self.video.read()
+        threading.Thread(target=self.update, args=()).start()
+
+    def __del__(self):
+        self.video.release()
+
+    def get_frame(self):
+        image = self.frame
+        ret, jpeg = cv2.imencode('.jpg', image)
+        return jpeg.tobytes()
+
+    def update(self):
+        while True:
+            (self.grabbed, self.frame) = self.video.read()
+
+
+cam = VideoCamera()
+
+
+def gen(camera):
+    while True:
+        frame = cam.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+
+@gzip.gzip_page
+def livefe(request):
+    try:
+        return StreamingHttpResponse(gen(VideoCamera()), content_type="multipart/x-mixed-replace;boundary=frame")
+    except:
+        pass
